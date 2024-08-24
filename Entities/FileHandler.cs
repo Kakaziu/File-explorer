0a42a39a13a0b132670513711e7e63736b21581b
Kauã <kaua.borba03@gmail.com>
2024-08-24 01:55:43 -0300

    Create method read file

--- Entities/FileHandler.cs
+++ Entities/FileHandler.cs
@@ -1,11 +1,25 @@
+using System.Text;
+
 namespace Entities {
   class FileHandler {
     public string Path {get; private set;}
 
-    public void CreateFile(string folder) {
-      using(StreamWriter sw = File.AppendText(Path + @"\" + folder)) {
-
+    public void CreateFile(string fileName, string content) {
+      using(StreamWriter sw = File.AppendText(Path + @"\" + fileName)) {
+        sw.WriteLine(content);
       }
+    }
+
+    public string ReadFile(string fileName) {
+      StringBuilder sb = new StringBuilder();
+      using(StreamReader sr = File.OpenText(Path + @"\" + fileName)) {
+        while(!sr.EndOfStream) {
+          sb.Append(sr.ReadLine());
+          sb.AppendLine();
+        }
+      }
+
+      return sb.ToString();
     }
   }
 }
