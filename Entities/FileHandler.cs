4e3e170b2b56512b5c93d5f8ea4a2e2c91265e38
Kauã <kaua.borba03@gmail.com>
2024-08-24 02:19:42 -0300

    create method get files

--- Entities/FileHandler.cs
+++ Entities/FileHandler.cs
@@ -3,7 +3,6 @@ using System.Text;
 namespace Entities {
   class FileHandler {
     public string Path {get; private set;}
-
     public void CreateFile(string fileName, string content) {
       using(StreamWriter sw = File.AppendText(Path + @"\" + fileName)) {
         sw.WriteLine(content);
@@ -20,6 +19,18 @@ namespace Entities {
       }
 
       return sb.ToString();
+    }
+
+    public List<string> GetFilesName() {
+      IEnumerable<string> files = Directory.EnumerateDirectories(Path + "*.*" + SearchOption.AllDirectories);
+      var filesName = new List<string>();
+    
+      foreach(var file in files) {
+        string[] directoryElements = file.Split(@"\");
+        filesName.Add(directoryElements[directoryElements.Length - 1]);
+      }
+
+      return filesName;
     }
   }
 }
